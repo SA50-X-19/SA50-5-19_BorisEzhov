665334c118b8596bd648a09af9a5c732bf245ab4
Boris <userNameIsBoris@gmail.com>
2021-04-06 11:43:43 +0300

    fix: add some JS code to adjust background size on window resize

---
+++ wildlife/script.js
@@ -0,0 +1,13 @@
+const backgroundWidth = 1440;
+
+window.onload = init;
+window.onresize = resizeBackground;
+
+// Functions
+function init() {
+    resizeBackground;
+}
+
+function resizeBackground() {
+    document.body.style.backgroundSize = window.innerWidth > backgroundWidth ? "contain" : "auto";
+}
